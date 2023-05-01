2d4abd5c1a91e17306ca470c291f70c810e3b165
Abdullah ONATLI <abdullah.onatli@gmail.com>
2023-05-01 22:29:48 +0300

    C# code

---
+++ C#/AkilliYatak/AkilliYatak/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace AkilliYatak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cmb_Ports = new System.Windows.Forms.ComboBox();
+            this.btn_PortSelect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_Position = new System.Windows.Forms.Label();
+            this.lbl_Time = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AkilliYatak.Properties.Resources.Fowler;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 127);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cmb_Ports
+            // 
+            this.cmb_Ports.FormattingEnabled = true;
+            this.cmb_Ports.Location = new System.Drawing.Point(51, 12);
+            this.cmb_Ports.Name = "cmb_Ports";
+            this.cmb_Ports.Size = new System.Drawing.Size(121, 21);
+            this.cmb_Ports.TabIndex = 1;
+            // 
+            // btn_PortSelect
+            // 
+            this.btn_PortSelect.Location = new System.Drawing.Point(178, 10);
+            this.btn_PortSelect.Name = "btn_PortSelect";
+            this.btn_PortSelect.Size = new System.Drawing.Size(75, 23);
+            this.btn_PortSelect.TabIndex = 2;
+            this.btn_PortSelect.Text = "Seç";
+            this.btn_PortSelect.UseVisualStyleBackColor = true;
+            this.btn_PortSelect.Click += new System.EventHandler(this.btn_PortSelect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ports:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pozisyon:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Süre:";
+            // 
+            // lbl_Position
+            // 
+            this.lbl_Position.AutoSize = true;
+            this.lbl_Position.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Position.Location = new System.Drawing.Point(372, 15);
+            this.lbl_Position.Name = "lbl_Position";
+            this.lbl_Position.Size = new System.Drawing.Size(92, 18);
+            this.lbl_Position.TabIndex = 6;
+            this.lbl_Position.Text = "POZİSYON";
+            // 
+            // lbl_Time
+            // 
+            this.lbl_Time.AutoSize = true;
+            this.lbl_Time.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Time.Location = new System.Drawing.Point(372, 50);
+            this.lbl_Time.Name = "lbl_Time";
+            this.lbl_Time.Size = new System.Drawing.Size(49, 18);
+            this.lbl_Time.TabIndex = 6;
+            this.lbl_Time.Text = "00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 358);
+            this.Controls.Add(this.lbl_Time);
+            this.Controls.Add(this.lbl_Position);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_PortSelect);
+            this.Controls.Add(this.cmb_Ports);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmb_Ports;
+        private System.Windows.Forms.Button btn_PortSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_Position;
+        private System.Windows.Forms.Label lbl_Time;
+    }
+}
+
---
+++ C#/AkilliYatak/AkilliYatak/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace AkilliYatak
+{
+    public partial class Form1 : Form
+    {
+        string positionTime;
+        int minute = 0;
+        int second = 0;
+        string lastposition;
+        string position;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cmb_Ports.Items.Add(port);
+            }
+        }
+
+        private void btn_PortSelect_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = cmb_Ports.SelectedItem.ToString();
+            serialPort1.Open();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+        private void positionChaged(string newposition)
+        {
+            if (newposition != position)
+            {
+                lastposition = position;
+                position = newposition;
+                positionTime = "00:00";
+
+                switch (newposition)
+                {
+                    case "rlateral":
+                        pictureBox1.Image = AkilliYatak.Properties.Resources.SagLateral;
+                        break;
+                    case "llateral":
+                        pictureBox1.Image = AkilliYatak.Properties.Resources.SolLateral;
+
+                        break;
+                    case "supine":
+                        pictureBox1.Image = AkilliYatak.Properties.Resources.Supine;
+
+                        break;
+                    case "fowler":
+                        pictureBox1.Image = AkilliYatak.Properties.Resources.Fowler;
+
+                        break;
+                    case "empty":
+                        pictureBox1.Image = null;
+                        break;
+
+
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            second++;
+            if (second > 59)
+            {
+                second = 0;
+                minute++;
+            }
+            positionTime = minute.ToString() + ":" + second.ToString();
+            lbl_Time.Text = positionTime;
+            positionChaged(decodeData(serialPort1.ReadLine()));
+            
+        }
+
+        private string decodeData(string data)
+        {
+            switch (data)
+            {
+                case "0000000000000000000000001":
+                    return "rlateral";
+                    
+                case "0000000000000000000100000":
+                    return "llateral";
+
+                case "0000000000000010000000000":
+                    return "spine";
+
+                case "0000100000000000000000000":
+                    return "fowler";
+
+                default:
+                    return "empty";
+
+
+            }
+        }
+    }
+}
